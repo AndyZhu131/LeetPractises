72b267433204986accad1b8bdaa8307aee238f89
Andy <charlotte131498349@gmail.com>
2025-08-06 17:40:33 -0400

    70 completed

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int) -> int:
+        step = []
+        for i in range(n):
+            if i == 0: step.append(1)
+            elif i == 1: step.append(2)
+            else:
+                newStep = step[0] + step[1]
+                step.pop(0)
+                step.append(newStep)
+        return step[-1]
         
 # @lc code=end
 
