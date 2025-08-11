7fc717a4ecf69d726d08bd8fc57ba1bcf60bcd56
Xiyuan Zhu <xiyuan.zhu@makegoodfood.ca>
2025-08-11 19:56:00 -0400

    unfinished 49

---
+++ 49.group-anagrams.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = {}
+        for s in strs:
+            sortedS = "".join(s.sort())
+            if sortedS in dict:
+                dict[sortedS].append(s)
+            else:
+                dict[sortedS] = [s]
+        twoD = []
+        for l in dict:
+            twoD.append(l)
+        return 0
+        
+# @lc code=end
+
